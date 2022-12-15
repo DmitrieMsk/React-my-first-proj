ee656a75464440148d76af431a0a1c65862b282a
DmitryMoskvin <moskvin_03@mail.ru>
2022-12-15 23:20:49 +0600

    first commit for this program

---
+++ src/Hello.js
@@ -0,0 +1,23 @@
+import React, {Component} from "react";
+export default class Hello extends Component 
+
+{
+    constructor(){
+        super();
+        this.state = {color: "red", digit: 0};
+
+    }
+    componentDidMount(){ 
+        setTimeout(() => {
+            this.setState({digit: 10}); }, 2000);
+    }
+    render(){
+        return<div>
+            <div onClick={() => {this.setState( {color: "yellow"})} } style={{backgroundColor: this.state.color}}>
+                Hello
+            </div>
+            <div>{this.state.digit}</div>
+            </div>
+
+    }
+}
